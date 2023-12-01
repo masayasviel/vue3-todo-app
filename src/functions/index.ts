9d694c7c849abb4bad81a28cc82675d1b97f9bbd
masayasviel <33654883+masayasviel@users.noreply.github.com>
2023-12-02 01:53:44 +0900

    :art:

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,17 +1,23 @@
 import {ItemStateLiteral, TaskInterface} from '@/interfaces/task.interface';
 import {ENDPOINT} from '@/constants';
 
+/** タスク一覧レスポンス */
 interface TaskListResponse {
   notStartedYetCategory: TaskInterface[];
-  wipCtegory: TaskInterface[];
+  wipCategory: TaskInterface[];
   finishCategory: TaskInterface[];
 }
 
+/** タスク一覧取得 */
 export async function getTaskList(): Promise<TaskListResponse> {
   const data = await fetch(ENDPOINT);
   return data.json();
 }
 
+/**
+ * タスク作成
+ * @param title タスクタイトル
+ */
 export async function createNewTask(title: string): Promise<void> {
   const body = {
     title: title,
@@ -26,6 +32,11 @@ export async function createNewTask(title: string): Promise<void> {
   });
 }
 
+/**
+ * タスク状態更新
+ * @param uuid タスクUUID
+ * @param status タスク新状態
+ */
 export async function updateStatus(uuid: string, status: ItemStateLiteral): Promise<void> {
   await fetch(`${ENDPOINT}/${uuid}/status`, {
     method: 'PATCH',
@@ -38,6 +49,10 @@ export async function updateStatus(uuid: string, status: ItemStateLiteral): Prom
   });
 }
 
+/**
+ * タスク削除
+ * @param uuid タスクUUID
+ */
 export async function deleteTask(uuid: string): Promise<void> {
   await fetch(`${ENDPOINT}/${uuid}`, {
     method: 'DELETE',
